9df1c75974761d4d8adb5eeb2020985bed79a470
Joan Camps <kmps84@gmail.com>
2023-12-03 13:21:06 +0100

    add a lined variant pdf

---
+++ lined-pdf.py
@@ -0,0 +1,38 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    topic = row["Topic"]
+    pages = row["Pages"]
+
+#  Set the header
+    pdf.add_page()
+    pdf.set_font(family="Helvetica", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)  # set text color to grey
+    pdf.cell(0, 12, topic, align="L", border=0, new_x="LMARGIN", new_y="NEXT")
+    pdf.set_draw_color(100, 100, 100)
+    pdf.set_line_width(1)
+    pdf.line(10, 21, 200, 21)
+
+#  Set the footer
+    pdf.set_y(-15)
+    pdf.set_font(family="Helvetica", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(0, 5, topic, align="R")
+
+    for i in range(pages - 1):
+        pdf.add_page()
+
+        #  Set the footer
+        pdf.set_y(-15)
+        pdf.set_font(family="Helvetica", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(0, 5, topic, align="R")
+
+
+pdf.output("output.pdf")
